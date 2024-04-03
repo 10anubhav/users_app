6f9492df962100414418f7ecd17acbfd087ec6bc
Anubhav Bhatnagar <2019anubhavbhatnagar@gmail.com>
2024-04-04 01:25:47 +0530

    Added a functionality in driver app - notify for a new trip request

---
+++ lib/methods/push_notification_service.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:users_app/appInfo/app_info.dart';
+import 'package:users_app/global/global_var.dart';
+import 'package:http/http.dart' as http;
+
+class PushNotificationService
+{
+  static sendNotificationToSelectedDriver(String deviceToken, BuildContext context, String tripID) async
+  {
+    String dropOffDestinationAddress = Provider.of<AppInfo>(context, listen: false).dropOffLocation!.placeName.toString();
+    String pickUpAddress = Provider.of<AppInfo>(context, listen: false).pickUpLocation!.placeName.toString();
+
+    Map<String, String> headerNotificationMap =
+    {
+      "Content-Type": "application/json",
+      "Authorization": serverKeyFCM,
+    };
+
+    Map titleBodyNotificationMap =
+    {
+      "title": "NET TRIP REQUEST from $userName",
+      "body": "PickUp Location: $pickUpAddress \nDropOff Location: $dropOffDestinationAddress",
+    };
+
+    Map dataMapNotification =
+    {
+      "click_action": "FLUTTER_NOTIFICATION_CLICK",
+      "id": "1",
+      "status": "done",
+      "tripID": tripID,
+    };
+
+    Map bodyNotificationMap =
+    {
+      "notification": titleBodyNotificationMap,
+      "data": dataMapNotification,
+      "priority": "high",
+      "to": deviceToken,
+    };
+
+    await http.post(
+      Uri.parse("https://fcm.googleapis.com/fcm/send"),
+      headers: headerNotificationMap,
+      body: jsonEncode(bodyNotificationMap),
+    );
+  }
+}
